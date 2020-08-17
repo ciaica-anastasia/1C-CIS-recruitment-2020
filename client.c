8bda3b48a7c53b9a55380d11dcb96edfc44402b1
ciaica-anastasia <48627245+ciaica-anastasia@users.noreply.github.com>
2020-08-17 15:53:46 +0300

    Add files via upload

--- client.c
+++ client.c
@@ -14,37 +14,43 @@ int main(int argc, char **argv)
     int portno = 0;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-   
+
     char message[MAX_BUF_SIZE] = { 0 };
-    
+
+    //Fill block of memory
     memset((void*)&serv_addr, 0, sizeof(serv_addr));
-    
+
     if (argc < 3)
     {
         printf("usage: %s hostname port\n", argv[0]);
         return 0;
     }
-    
+
     portno = atoi(argv[2]);
+
+    //Create a socket point
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    
+
     if (sockfd < 0)
     {
         perror("socket");
         return 0;
     }
-    
+
     server = gethostbyname(argv[1]);
     if (server == NULL)
     {
         printf("No such host\n");
         return 0;
     }
-    
+
     serv_addr.sin_family = AF_INET;
+
+    //Copy block of memory
     memcpy((char*)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
-    
+
+    //Connect to the server
     if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
     {
         perror("connect");
@@ -53,35 +59,38 @@ int main(int argc, char **argv)
 
     recv(sockfd, message, MAX_BUF_SIZE - 1, 0);
     printf("%s\n", message);
-    
+
+    //Ask for a message from the user, this message will be read by server
     printf("Message: ");
     fgets(message, MAX_BUF_SIZE - 1, stdin);
-    
+
     while (strcmp(message, "quit\n\0") != 0)
     {
+        //Send message to the server
         if (write(sockfd, message, strlen(message)) < 0)
         {
             perror("write");
             return 0;
         }
-        
+
         if (message[0] == ';')
         {
-            if (read(sockfd, message, MAX_BUF_SIZE - 1) < 0)
+            //Read server response
+            if (recv(sockfd, message, MAX_BUF_SIZE - 1, SO_NOSIGPIPE) < 0)
             {
                 perror("read");
                 return 0;
             }
             printf("%s\n", message);
         }
-        
+
         printf("Message: ");
-        
+
         memset(message, 0, MAX_BUF_SIZE);
         fgets(message, MAX_BUF_SIZE - 1, stdin);
     }
-    
+
     close(sockfd);
-    
+
     return 0;
 }
--- server.c
+++ server.c
@@ -1,145 +1,149 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
+#include <string.h> //strlen
+#include <stdlib.h> //strlen
+#include <pthread.h> //for threading
+#include <stdbool.h>
 
-#define MESSAGE_FILE "./messages"
+const size_t MAX_BUF_SIZE = 2048;
+const size_t MAX_MSG_SIZE = 2000;
 
-void *connection_handler(void *);
- 
+#define panic(...) { fprintf(stderr, __VA_ARGS__); fflush(stderr); exit(1); }
+
+//mutex for writing to file
+pthread_mutex_t lock;
 
 const size_t clients_amount = 5;
-pthread_t clients[clients_amount];
+pthread_t clients[] = {0, 0, 0, 0, 0};
 
-int main(int argc , char *argv[])
-{
-    int sockfd = 0;
-    int client_sock = 0;
-    int fd_messages = 0;
-    int params[] = { 0, 0, 0 }; // sockfd, fd_message, client_num
-    
-    struct sockaddr_in server;
-    struct sockaddr_in client;
-    
-    size_t sockaddr_in_size = sizeof(struct sockaddr_in);
-    
-    sockfd = socket(AF_INET , SOCK_STREAM , 0);
-    if (sockfd == -1)
-    {
-        perror("socket");
-        return 0;
-    }
-    //puts("Socket created");
-     
+void *connection_handler(void *params);
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) panic("Usage: %s [port]\n", argv[0]);
+
+    int port =  atoi(argv[1]);
+
+    int params[] = {0, 0};
+    int server_fd, client_fd;
+    int err;
+    struct sockaddr_in server, client;
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) panic("Could not create socket\n");
+
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(9998);
-     
-    if(bind(sockfd, (struct sockaddr *)&server, sizeof(server)) < 0)
-    {
-        perror("bind");
-        return 0;
-    }
-    //puts("bind done");
-     
-    listen(sockfd, clients_amount);
-    fd_messages = open(MESSAGE_FILE, O_RDWR | O_APPEND | O_CREAT);
-    
-    //puts("Waiting for incoming connections...");
-    
-    while((client_sock = accept(sockfd, (struct sockaddr *)&client,
-                                (socklen_t*)&sockaddr_in_size)))
-    {
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    err = bind(server_fd, (struct sockaddr *) &server, sizeof(server));
+    if (err < 0) panic("Could not bind socket\n");
+
+    err = listen(server_fd, 128);
+    if (err < 0) panic("Could not listen on socket\n");
+
+    printf("Server is listening on %d\n", port);
+
+    while (1) {
+        socklen_t client_len = sizeof(client);
+        client_fd = accept(server_fd, (struct sockaddr *) &client, &client_len);
+
+        if (client_fd < 0) panic("Could not establish new connection\n");
+
         puts("[Connection accepted]");
-        
-        for (int i = 0; i < clients_amount; ++i)
-        {
-            if (clients[i] == 0)
-            {
-                params[0] = sockfd;
-                params[1] = fd_messages;
-                params[2] = i;
-                
-                //printf("[in main] %d %d %d\n", sockfd, fd_messages, i);
-                
-                if(pthread_create(&clients[i], NULL,
-                                  connection_handler, (void*)params) < 0)
-                {
-                    perror("thread");
+        for (int i = 0; i < clients_amount; ++i) {
+            if (clients[i] == 0) {
+                params[0] = client_fd;
+                params[1] = i;
+
+                pthread_attr_t attr; /* thread attrs */
+
+                pthread_attr_init(&attr);
+
+                if (pthread_create(&clients[i], &attr, connection_handler, (void *) params) > 0) {
+                    panic("thread");
                 }
                 break;
             }
         }
-        
-        //pthread_join(thread_id, NULL);
-        //puts("Handler assigned");
+
     }
-    
-    close(fd_messages);
-    
-    if (client_sock < 0)
-    {
-        perror("accept");
-        return 0;
-    }
-     
+
     return 0;
 }
- 
 
-void *connection_handler(void *params)
-{
-    const size_t MAX_BUF_SIZE = 2048;
-    const size_t MAX_MSG_SIZE = 2000;
-    
-    int sockfd = ((int*)params)[0];
-    int fd_message = ((int*)params)[1];
-    int client_num = ((int*)params)[2];
-    
+
+
+bool message_is_get(const char * msg){
+    return msg[0] == 'g' && msg[1] == 'e' && msg[2] == 't';
+}
+
+
+//This will handle connection for each client
+void *connection_handler(void *params) {
+    int sockfd = ((int *) params)[0];
+    int client_num = ((int *) params)[1];
+
+    // read size
     ssize_t read_size = 0;
-    char write_buf[MAX_BUF_SIZE] = { 0 };
+    char write_buf[MAX_BUF_SIZE] = {0};
     char client_message[MAX_MSG_SIZE] =
-        "Your connection is established. Now you can send a mesage.\n";
+            "Your connection is established. Now you can send a mesage.\n";
 
-    printf("[in thread] %d %d %d\n", sockfd, fd_message, client_num);
-    
-    write(sockfd, client_message, strlen(client_message));
-    
-    while((read_size = recv(sockfd, client_message, MAX_MSG_SIZE - 1, 0)) > 0)
-    {
+    send(sockfd, client_message, strlen(client_message), SO_NOSIGPIPE); // CHANGE TO MSG_NOSIGNAL ON LINUX
+
+    //Receive a message from client
+    while (1) {
+        read_size = recv(sockfd, client_message, MAX_BUF_SIZE, SO_NOSIGPIPE);
+
+        if (!read_size) break; // done reading
+        if (read_size < 0) panic("Client read failed\n");
+
         client_message[read_size] = '\0';
-        //write(sockfd, client_message, strlen((const char*)client_message));
-        
-        if (client_message[0] == ';')
-        {
-            
-        }
-        else
-        {
+
+        //send(sockfd, client_message, read_size, SO_NOSIGPIPE);
+
+        if(message_is_get(client_message)){
+            int get_num = atoi(&client_message[4]);
+
+            pthread_mutex_lock(&lock);
+            FILE *fp = fopen("messages", "r");
+
+            char send_msg[MAX_MSG_SIZE];
+
+            while(fgets(send_msg, MAX_MSG_SIZE + 5, fp) != NULL){
+                if((send_msg[0] - '0') == get_num)
+                    send(sockfd, send_msg, strlen(send_msg), SO_NOSIGPIPE); // CHANGE TO MSG_NOSIGNAL ON LINUX
+            }
+
+            fclose(fp);
+            pthread_mutex_unlock(&lock);
+
+        } else {
             sprintf(write_buf, "%d - %s", client_num, client_message);
-            write(fd_message, write_buf, strlen(write_buf));
+
+            pthread_mutex_lock(&lock);
+
+            FILE *fp = fopen("messages", "a");
+
+            fprintf(fp,"%s",write_buf);
+
+            fclose(fp);
+            pthread_mutex_unlock(&lock);
         }
+
+
+
         memset(client_message, 0, MAX_MSG_SIZE);
+
     }
-     
-    if(read_size == 0)
-    {
-        puts("[Client disconnected]");
-        fflush(stdout);
-    }
-    else if(read_size == -1)
-    {
-        perror("recv");
-    }
-         
+
+
+    puts("[Client disconnected]");
+    fflush(stdout);
+
+
     clients[client_num] = 0;
-    
     return 0;
 }
